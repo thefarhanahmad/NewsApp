4a7cf94b3e1fa4c0674119d48593b8f13daef386
farhan <akhtarfarhan281@gmail.com>
2024-10-03 12:58:08 +0530

    image crousel

--- src/Components/MainPage/ImageCrousel.jsx
+++ src/Components/MainPage/ImageCrousel.jsx
@@ -34,7 +34,7 @@ function ImageCrousel() {
     <>
       <div className="ImageCarouselContainer">
         <Carousel className="ImageCarsouel" arrows infinite={false}>
-          {photo &&
+          {/* {photo &&
             photo.map((img) => {
               console.log("IMG", img);
               return (
@@ -75,6 +75,55 @@ function ImageCrousel() {
                     <img src={img.img} alt="" />
                   </div>
                 </>
+              );
+            })} */}
+          {photo &&
+            photo.map((img) => {
+              console.log("IMG", img);
+              const handleImageClick = () => {
+                // URL validation regex
+                const urlPattern = new RegExp(
+                  "^(https?:\\/\\/)?(www\\.)?([a-zA-Z0-9]+\\.[a-zA-Z]{2,})([\\/\\w\\.-]*)*\\/?$"
+                );
+
+                // Check if the URL is valid
+                const isValidUrl = urlPattern.test(img?.url);
+
+                // Redirect to the valid URL or fallback URL
+                const redirectUrl = isValidUrl ? img.url : img.img;
+
+                window.open(redirectUrl, "_blank");
+              };
+
+              return (
+                <div key={img._id}>
+                  <h3
+                    className="main-page-video-heading2"
+                    style={{
+                      padding: "6px",
+                      fontSize: "2rem",
+                      color: "white",
+                      bottom: "30px",
+                      textAlign: "center",
+                      borderRadius: "10px",
+                      overflow: "hidden",
+                      marginBottom: "5px",
+                    }}
+                  >
+                    {img?.text}
+                  </h3>
+                  <div
+                    style={{
+                      cursor: "pointer",
+                      position: "relative",
+                      width: "100%",
+                    }}
+                    className="photoGallery-card"
+                    onClick={handleImageClick}
+                  >
+                    <img src={img.img} alt="" />
+                  </div>
+                </div>
               );
             })}
         </Carousel>
