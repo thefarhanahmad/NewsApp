ee5e33904a1bc760534fb52fb3fcf3691e0cc0a7
farhan <akhtarfarhan281@gmail.com>
2024-11-06 15:51:02 +0530

    header

--- src/Components/Global/MobileHeader.jsx
+++ src/Components/Global/MobileHeader.jsx
@@ -168,21 +168,19 @@ const MobileHeader = ({ listitem }) => {
           </div>
 
           <ul className="mobilevisibleNavItemsUlChild">
-            
-              <li
-              // style={{ cursor: "pointer" }}
-              // onClick={() => {
-              //   Navigation("/live");
-              //   setHambergClicked(false);
-              // }}
-              >
-                <Link to="/live">
-                  <PlayCircleOutlined
-                    style={{ marginLeft: "10px", fontSize: "18px" }}
-                  />
-                </Link>
-              </li>
-            
+            <li
+            // style={{ cursor: "pointer" }}
+            // onClick={() => {
+            //   Navigation("/live");
+            //   setHambergClicked(false);
+            // }}
+            >
+              <Link to="/live">
+                <PlayCircleOutlined
+                  style={{ marginLeft: "10px", fontSize: "18px" }}
+                />
+              </Link>
+            </li>
 
             <li>
               <Link to="/itempage?item=Web%20Story">
@@ -195,7 +193,7 @@ const MobileHeader = ({ listitem }) => {
               </Link>
             </li>
             <li>
-              <Link to="http://localhost:5173/itempage2?newsType=videos">
+              <Link to="/itempage2?newsType=videos">
                 <FaPhotoVideo size={18} />
               </Link>
             </li>
